5efad05d41f54588487781f881331b7eacb5a287
wajihnajjar <wajih.najjar@esprit.tn>
2023-08-10 09:11:47 +0100

    Adding Posting Limit for every user that 2 post per second

--- controller/userController.js
+++ controller/userController.js
@@ -1,21 +1,27 @@
 const { head } = require("../routes/userRouter")
 var cron = require('node-cron');
 
-cron.schedule(' * * * * 4', () => {
 
-});
   
 $: map = new Map()
 let  weeklyClaim = new Map()
+let postUsers = new Map()
 cron.schedule(' * * * * 4', () => {
 weeklyClaim.clear()
 });
 cron.schedule(' * 00 * * *', () => {
-    map.clear()
-    });
     
+    weeklyClaim.clear()
+ });
+    
+ cron.schedule(' 1 * * * * *', () => {
+    console.log("Here")
+    postUsers.clear()
+ });
+
 
 exports.signUp = (req,res)=>{
+    console.log("Test")
     for (const [key , value] of map){
     console.log(key ," ",value)
 }
@@ -42,8 +48,15 @@ exports.signUp = (req,res)=>{
 
 
 exports.postBlog = (req,res)=>{
-
-
+const user = req.session.user.username 
+const Value  = postUsers.get(user)
+if(Value>=2){
+  res.send("You Surpasse your limit ")  
+}
+else {
+    Value ==undefined? postUsers.set(user,1) : postUsers.set(user,Value+1)
+    res.send("Done Posting ")
+}
 }
 
 exports.claimReward = (req,res)=>{
@@ -57,7 +70,9 @@ exports.claimReward = (req,res)=>{
      }
      else {
         value==undefined ? weeklyClaim.set(device , 1) : weeklyClaim.set(device,value+1)
-     }
+        res.send("Done Posting ")
+
+    }
 
 
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,5 +3,6 @@ const Router = express.Router()
 const userController = require("../controller/userController")
 Router.route("/signup").post(userController.signUp)
 Router.route("/claim").post(userController.claimReward)
+Router.route("/post" ).post(userController.postBlog)
 
 module.exports = Router
