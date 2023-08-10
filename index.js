996dc4925e85afb5489972ae4bae36d38d47fa57
wajihnajjar <wajih.najjar@esprit.tn>
2023-08-10 19:17:34 +0100

    Adding Swagger Api

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const User  =require("./routes/userRouter")
 var device = require('express-device');
 var session = require('express-session');
 const bodyParser = require("body-parser");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 var FileStore = require('session-file-store')(session);
 var fileStoreOptions = {};
 
